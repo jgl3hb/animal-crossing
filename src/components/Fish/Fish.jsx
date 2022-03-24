704323965678b2f2d34ab86a46e7414be1ce1ac8
austinjs <ajsprunger@gmail.com>
2022-03-24 14:53:37 -0600

    add fish button

--- src/components/Fish/Fish.jsx
+++ src/components/Fish/Fish.jsx
@@ -3,16 +3,17 @@ import { Link } from "react-router-dom";
 const Fish = ({fish}) => {
   return (
     <>
-    <Link
-      to='/fish'
-      state={{fish}}
-      className='card-link'
-    >
-      <div className="fish">
-        <img src={fish.icon_uri} alt="fish" />
-        <p>{fish.name['name-USen']}</p>
-      </div>
-    </Link>
+    <div className="fish">
+      <Link
+        to='/fish'
+        state={{fish}}
+        className='card-link'
+      >
+          <img src={fish.icon_uri} alt="fish" />
+          <p>{fish.name['name-USen']}</p>
+      </Link>
+      <button style={{'backgroundColor': 'orange'}} className="btn">Add to Collection</button>
+    </div>
     </>
   );
 }
